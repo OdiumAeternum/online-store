3cebaf690d42233b69280abe43e394e99f1314cc
OdiumAeternum <71345115+OdiumAeternum@users.noreply.github.com>
2021-12-20 21:07:16 +0300

    Delete server directory

--- server/routes/brandRouter.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express')
-const router = new Router()
-
-//для того, чтобы создать бренд
-router.post('/',)
-//для того, чтобы получать бренд
-router.get('/',)
-
-module.exports = router
--- server/routes/typeRouter.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express')
-const router = new Router()
-
-//для того, чтобы создать тип
-router.post('/',)
-//для того, чтобы получать тип
-router.get('/',)
-
-module.exports = router
